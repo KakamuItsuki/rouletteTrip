3a612e85ca6507eff20328195711e47619dcb5b4
KakamuItsuki <ringo.nk.os.1030@icloud.com>
2024-07-01 22:34:28 +0900

    リストのレイアウト調整

--- rouletteTrip7/src/app/tab2/list/list.component.ts
+++ rouletteTrip7/src/app/tab2/list/list.component.ts
@@ -14,6 +14,7 @@ export class ListComponent  implements OnInit {
 
   ngOnInit() {
     this.missions = this.bingoService.getMissions();
+    this.isShow = this.bingoService.getIsShow();
   }
   
   onClickCard(){
