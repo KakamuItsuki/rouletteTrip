15718e277dfa0ba7569955f3e5ba5da26abff806
KakamuItsuki <ringo.nk.os.1030@icloud.com>
2024-07-02 22:44:09 +0900

    テスト

--- rouletteTrip7/src/app/tab1/tab1.page.ts
+++ rouletteTrip7/src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  style = {'height': '400px', 'transition':'2.5s'}
+  //wheel = document.querySelector(".js-slot_wheel_inner")
+  wheel: HTMLElement | null = document.getElementById( 'js-slot_wheel_inner' );
+  // wheel: HTMLCollectionOf<Element> = document.getElementsByClassName( 'js-slot_wheel_inner' );
   constructor() {}
 
+  onClickStart(){
+    console.log("ok1");
+    this.style = {'height': '200px'}
+    
+    if(this.wheel !== null){
+      console.log("ok2");
+      this.wheel.style.transform = `translate3d(0px, 100px, 0px)`;
+    }
+    
+  }
 }
