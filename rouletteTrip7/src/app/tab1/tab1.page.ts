fc98ea79f4d896b22969f600bbb6a659976503b8
KakamuItsuki <ringo.nk.os.1030@icloud.com>
2024-07-03 15:23:20 +0900

    リピートなしスロット

--- rouletteTrip7/src/app/tab1/tab1.page.ts
+++ rouletteTrip7/src/app/tab1/tab1.page.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material/core';
+
+
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('changeColor', [
+    state(
+      'blue',
+      style({
+      background: 'blue',
+      top: '-144px',
+      //position: 'fixed',
+      position: 'absolute',
+      })
+    ),
+    state(
+      'red',
+      style({
+      background: 'red',
+      top: '0px',
+      //position: 'fixed',
+      position: 'absolute',
+      })
+    ),
+    transition('blue => red', [animate('0.5s')]),
+    transition('red => blue', [animate('3s')]),
+    ]),
+  ]
 })
+
 export class Tab1Page {
-
+  slotState = 'stop';
+ 
   constructor() {}
+  isBlue = true;
 
+  changeColor() {
+    this.isBlue = !this.isBlue;
+  }
+
+  onClickStart(){
+    this.slotState = 'start';
+  }
+  onClickStop(){
+    this.slotState = 'stop';
+  }
+  onClickDefolte(){
+    this.slotState = 'defolte'
+  }
 }
